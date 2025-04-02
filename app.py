b83db9591a9155715cad7500f111dfcee0c34ba0
shaiknasirahamed <shaiknasirahamed@gmail.com>
2025-04-02 04:54:22 +0000

    updated app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 def home():
     return "Hello, Jenkins Deployment Successful!"
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='3.12.107.215', port=5000)
 
